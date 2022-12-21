80f454b838e90ef36bf36c81234427230c29e119
ByanUp <120808383+ByanUp@users.noreply.github.com>
2022-12-21 19:34:56 +0300

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,12 @@
+#var_a = 1
+#a = 2
+
+#var_b=input('Введите число: ')
+#print(var_b)
+
+print(input('Введите числа: '))
+print('Хороший выбор. А теперь сохраните их: ')
+print(input('var_c='))
+
+str_1="This is my first string of numbers"
+print(str_1)
