cd1e7c59190bc1306f5a387a98bfd166845d40ed
ByanUp <120808383+ByanUp@users.noreply.github.com>
2022-12-24 20:44:48 +0300

    Update task3.py

--- task3.py
+++ task3.py
@@ -1,6 +1,5 @@
-n = int(input('Введите число: '))
-n_str = str(n)
-n_str1 = n_str + n_str
-n_str2 = n_str1 + n_str
-summa_n = n + int(n_str1) + int(n_str2)
-print('Сумма n равно: ', summa_n)
+n = input('Введите число: ')
+n_str1 = n + n
+n_str2 = n_str1 + n
+new_n = int(n) + int(n_str1) + int(n_str2)
+print('Сумма n равно: ', new_n)
